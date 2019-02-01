008622d1cab027acfd120ea69144f1c41d836a30
Aleksei Tcysin <AlekseiTcysin@users.noreply.github.com>
2019-02-02 01:03:17 +0200

    Librarifier JSON-based version
    Sat Feb 2 00:31:13 2019 +0200, 35ff32d by Aleksei Tcysin
    Message: Added error detection procedures and unit tests, cleaned up documentation, added optional output_path parameter
    
    Fri Feb 1 22:57:38 2019 +0200, 2340b18 by Aleksei Tcysin
    Message: Additional documentation
    
    Fri Feb 1 22:50:41 2019 +0200, 91c097e by Aleksei Tcysin
    Message: Added proper module documentation
    
    Fri Feb 1 22:38:54 2019 +0200, 58a4513 by Aleksei Tcysin
    Message: Completed and debug-stepped through the procedure
    
    Sun Jan 27 14:56:06 2019 +0200, 3ff95b1 by Aleksei Tcysin
    Message: Changed .libr output to JSON file instead of simple txt
    
    Sat Jan 26 15:01:24 2019 +0200, c1a48a0 by Aleksei Tcysin
    Message: Finished the main function with text-based format

---
+++ project/player/app/librarifier/librarifier.py
@@ -0,0 +1,229 @@
+"""
+DESCRIPTION
+    Librarifier is a module used to create a Library file.
+    
+    The name of the newly generated file consists of the 'libr_' prefix,
+    name of the file without extension, and '.libr' extension. For
+    example, 'libr_hello.libr'.
+    
+    Library file itself is a JSON object which contains the following 
+    key-value pairs (in arbitrary order):
+        'hub_address': string, consists of IP:PORT combination
+        'chunk_size': int, used to split file into pieces
+        'hashes': array of hexadecimal hash strings of each piece
+        'file_size': int, in bytes
+        'file_name': string, includes actual name and extension
+
+FUNCTIONS
+    librarify(input_file, hub_address, chunk_size=2048, output_path=None)
+        Generates a .libr description of input_file.
+
+1 + 1 + 1 + 2 + 1 + 1
+"""
+
+import hashlib
+import json
+import math
+import os.path
+import socket
+import unittest
+
+
+# book (chunk) size, in bytes
+BOOK_SIZE = 2048
+EXTENSION = '.libr'
+PREFIX = 'libr_'
+
+
+# --- private functions ---
+def _bytes_to_sha1(chunk):
+    """Returns a SHA1 hexadecimal hash string for a chunk of bytes."""
+
+    hasher = hashlib.sha1()
+    hasher.update(chunk)
+    result = hasher.hexdigest()
+
+    return result
+
+def _verify_input_file(input_file):
+    """Raise errors if input_file is not a string or does not exist."""
+    
+    # raise TypeError if provided argument is not a string
+    if type(input_file) is not str:
+        raise TypeError('Provided input path is not a string.')
+
+    # raise ValueError if provided path is not a file 
+    if not os.path.isfile(input_file):
+        raise ValueError('Provided input is not a file or does not exist.')
+
+def _verify_hub_address(hub_address):
+    """Raise errors if argument is not a string or not a valid IPv4:PORT string."""
+
+    # raise TypeError if provided argument is not a string
+    if type(hub_address) is not str:
+        raise TypeError('Provided hub address is not a string.')
+
+    # split on double-dot to get IP address part and port
+    try:
+        ip, port = hub_address.split(':')
+
+        # check valid IPv4 address
+        socket.inet_aton(ip)
+
+        # check valid port
+        port_in_range = (int(port) >= 0 or int(port) <= 65535)
+
+        if not port_in_range:
+            raise Exception('Port is invalid')
+
+    except:
+        raise ValueError('Provided hub address is invalid.')
+
+def _verify_output_path(output_path):
+    """Raise errors if argument is not a string or not a valid path."""
+
+    # raise TypeError if provided argument is not a string
+    if type(output_path) is not str:
+        raise TypeError('Provided output path is not a string.')
+
+    # ValueError if path does not exist
+    # raise ValueError if provided path is not a file 
+    if not os.path.exists(output_path):
+        raise ValueError('Provided output path is not a valid path.')
+
+
+# --- public functions ---
+
+def librarify(input_file, hub_address, chunk_size=BOOK_SIZE, output_path=None):
+    """Takes a file, generates a .libr library file and returns its path.
+
+    Params:
+    ------
+    input_file -- a text or byte string giving the name of the file
+        Should include the path if the file isn't in the 
+        current working directory.
+    hub_address -- a string giving the address of the tracker and the port
+        For example, '192.168.1.1:7777' is a valid hub address.
+    chunk_size -- size, in bytes, of each piece, excluding the last one
+    """
+
+    # ERROR CHECKING AND HANDLING
+    # make sure input_file exists
+    _verify_input_file(input_file)
+    
+    # make sure hub address is OK (right type, is actually an IP:port)
+    _verify_hub_address(hub_address)
+
+    # make sure output_path is ok, if provided
+    _verify_output_path(output_path)
+
+
+    data = {'hub_address': hub_address, 'chunk_size': chunk_size}
+        
+    hashes = []
+    with open(input_file, 'rb') as infile:
+
+        while True:
+            # read next chunk of bytes, quit if nothing left
+            chunk_bytes = infile.read(chunk_size)
+
+            if not chunk_bytes:
+                break
+
+            # compute hash value of each book (chunk)
+            chunk_hash = _bytes_to_sha1(chunk_bytes)
+            # save these values
+            hashes.append(chunk_hash)
+    data['hashes'] = hashes
+
+    # get file size
+    file_size = os.path.getsize(input_file)
+    # make sure we correctly split the file in chunks
+    assert( len(hashes) == math.ceil(file_size/chunk_size) )
+    data['file_size'] = file_size
+
+    # get filename, save it, chop off extension
+    input_file_directory, file_name = os.path.split(input_file)
+    data['file_name'] = file_name
+    pure_name, _ = os.path.splitext(file_name)
+
+    # write meaningful information in library file
+    if output_path is None:
+        output_path = input_file_directory
+
+    output_path = os.path.join(output_path, PREFIX + pure_name + EXTENSION)
+
+    with open(output_path, 'w') as outfile:
+        json.dump(data, outfile, separators=(',', ': '))
+
+    # OUTPUT: the path to the newly created .libr file
+    return output_path
+
+
+# --- unit testing ---
+
+class TestHashingFunction(unittest.TestCase):
+    def test__bytes_to_sha1(self):
+        self.assertEqual(
+            _bytes_to_sha1(b'hello'),
+            'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d')
+
+class TestVerificationFunctions(unittest.TestCase):
+
+    def test__verify_input_file(self):
+        with self.assertRaises(TypeError):
+            _verify_input_file(42)
+
+        # check for incorrect path or non-existent file
+        with self.assertRaises(ValueError):
+            _verify_input_file('abracadabra')
+
+    def test__verify_hub_address(self):
+        
+        # check for non-string input
+        with self.assertRaises(TypeError):
+            _verify_hub_address(42)
+        
+        # check for gibberish
+        with self.assertRaises(ValueError):
+            _verify_hub_address('abracadabra')
+
+        # check for incorrect IPv4
+        with self.assertRaises(ValueError):
+            _verify_hub_address('1.1.2567.0:1234')
+
+        # check for missing port
+        with self.assertRaises(ValueError):
+            _verify_hub_address('192.168.1.1')
+
+        with self.assertRaises(ValueError):
+            _verify_hub_address('192.168.1.1:')
+
+        # check for port out of bounds
+        with self.assertRaises(ValueError):
+            _verify_hub_address('192.168.1.1:90000')
+
+        with self.assertRaises(ValueError):
+            _verify_hub_address('192.168.1.1:-1')
+
+    def test__verify_output_path(self):
+
+        # check for non-string input
+        with self.assertRaises(TypeError):
+            _verify_output_path(42)
+
+        # check for incorrect path or non-existent file
+        with self.assertRaises(ValueError):
+            _verify_input_file('abracadabra')  
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# testing the module
+    # informal code inspection
+    
+    # unit tests
+        # different files (mp3, jpeg, txt, py, etc.)
+        # invalid user input
+        # correctness of hashing
